4f4a3edce7e340601ecb00c22141821d3e86c5af
ionlungu <30807360+ionlungu@users.noreply.github.com>
2017-08-08 18:08:53 +0300

    Created by Coding Ground Client

---
+++ New Project-20170808/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+
+
+    public static void main(String[] args) {
+        Team t1 = new Team();
+        t1.setTeams();
+        
+        System.out.println();
+        
+        Goal goal = new Goal();
+        goal.scoreGoal();
+    }
+    
+}
