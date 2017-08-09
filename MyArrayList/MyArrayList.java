039c4e4f13f8054d3495becb703ae2c09d53e97e
ionlungu <30807360+ionlungu@users.noreply.github.com>
2017-08-09 09:50:38 +0300

    Created by Coding Ground Client

---
+++ MyArrayList/MyArrayList.java
@@ -0,0 +1,8 @@
+public class MyArrayList{
+    
+    ArrayList<Integer> aList = new ArrayList<Integer>(12);
+
+     public static void main(String []args){
+        System.out.println("test");
+     }
+}
