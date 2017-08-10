b1decae363686bfe256bffbc10378f52755b10ab
ionlungu <30807360+ionlungu@users.noreply.github.com>
2017-08-10 10:17:39 +0300

    Delete MyArrayList.java

--- MyArrayList/MyArrayList.java
+++
@@ -1,8 +0,0 @@
-public class MyArrayList{
-    
-    ArrayList<Integer> aList = new ArrayList<Integer>(12);
-
-     public static void main(String []args){
-        System.out.println("test");
-     }
-}
